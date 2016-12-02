371b39b4eac7ef30e2ae1ebca749781603efd72e
Kiana McNellis <kiana@kmcnellis.com>
2016-12-02 02:26:03 -0500

    Add saving & reading keys and canidates

---
+++ Pailler Evoting/genKey.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import paillier
+import paillier.paillier as p
+import rsa
+import pickle
+
+res = raw_input("Enter '1' for EB_paillier, '2' for EB_RSA, or '3' for BB_RSA: ")
+
+if (res == "1"): #EB_paillier
+    print "Generating keypair..."
+    priv, pub = p.generate_keypair(1024) #generate public and private keys
+    #public key gets given to voters to encrypt their votes
+
+    private_keyfile = open("keyserver/Private/EB_paillier.key", 'w')
+    public_keyfile = open("keyserver/Public/EB_paillier_public.key", 'w')
+
+    pickle.dump(priv,private_keyfile)
+    pickle.dump(pub,public_keyfile)
+
+if (res == "2"): #EB_RSA
+    print "Generating keypair..."
+    (priv, pub) = rsa.newkeys(1024)
+    #public key gets given to voters to encrypt their votes
+
+    private_keyfile = open("keyserver/Private/EB_RSA.key", 'w')
+    public_keyfile = open("keyserver/Public/EB_RSA_public.key", 'w')
+
+    pickle.dump(priv,private_keyfile)
+    pickle.dump(pub,public_keyfile)
+
+if (res == "3"): #EB_RSA
+    print "Generating keypair..."
+    (priv, pub) = rsa.newkeys(1024)
+    #public key gets given to voters to encrypt their votes
+
+    private_keyfile = open("keyserver/Private/BB_RSA.key", 'w')
+    public_keyfile = open("keyserver/Public/BB_RSA_public.key", 'w')
+
+    pickle.dump(priv,private_keyfile)
+    pickle.dump(pub,public_keyfile)
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Paillier-Evoting
 
 current example stuff / process is in paillier_test.py
+
+## Requirements:
+1. Update Pailler Evoting/paillier submodule (https://github.com/mikeivanov/paillier)
+2. ```pip install -r requirements.txt```
+
+## To run:
+1. ```cd Pailler\ Evoting```
+2. ```python evoting_main.py```
+3. ```python evoting_client.py```
